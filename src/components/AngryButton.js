005677a3bb954457458eac8330e65ca18efd2f53
Jeandre <jeandrev1414@gmail.com>
2022-08-08 14:04:05 -0600

    Implement handleClick function

--- src/components/AngryButton.js
+++ src/components/AngryButton.js
@@ -1,11 +1,17 @@
 import { useState } from "react";
 
 function AngryButton(props) {
-
   const [anger, setAnger] = useState(0);
 
+  const handleClick = (event) => {
+    if (anger < 1) {
+      setAnger(anger + 0.1);
+    } else {
+      setAnger(0);
+    }
+  }
   return (
-    <button className="AngryButton" style={{backgroundColor: `rgba(255,0,0,${anger})`}}>
+    <button onClick={handleClick} className="AngryButton" style={{backgroundColor: `rgba(255,0,0,${anger})`}}>
     {/* When the threshold is not reached */}
     { anger < 1 && <span>Don't click me too much! </span>}
     {/* When the threshold is not reached */}
