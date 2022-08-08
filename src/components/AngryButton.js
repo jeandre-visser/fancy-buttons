e2b7be2af08ba0dcbcd6553021f2dc08208dcce1
Jeandre <jeandrev1414@gmail.com>
2022-08-08 13:22:56 -0600

    Add inline button styling and conditional rendering

--- src/components/AngryButton.js
+++ src/components/AngryButton.js
@@ -1,11 +1,15 @@
 import { useState } from "react";
 
-function AngryButton() {
+function AngryButton(props) {
+
+  const [anger, setAnger] = useState();
 
   return (
-    <button className="AngryButton">
-    {/* When the threshold is not reached */}<span>Don't click me too much! </span>
-    {/* When the threshold is not reached */}<span>Rawr!</span>
+    <button className="AngryButton" style={{backgroundColor: `rgba(255,0,0,${anger})`}}>
+    {/* When the threshold is not reached */}
+    { anger < 1 && <span>Don't click me too much! </span>}
+    {/* When the threshold is not reached */}
+    { anger > 1 && <span>Rawr!</span> }
   </button>
   );
 }
