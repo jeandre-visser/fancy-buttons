b4603ae060d96f785bf0a745dd78bf1e059c7c1a
Jeandre <jeandrev1414@gmail.com>
2022-08-08 14:40:24 -0600

    Update to use props values

--- src/components/LightSwitchButton.js
+++ src/components/LightSwitchButton.js
@@ -1,7 +1,8 @@
 import { useState } from "react";
 
 function LightSwitchButton(props) {
-  const [light, setLight] = useState('off');
+
+const {light, setLight} = props;
 
   const handleClick = () => {
     setLight(light === "on" ? "off" : "on");
