46986ea31ed51129306bc1c77bf82dafacf60d30
Jeandre <jeandrev1414@gmail.com>
2022-08-08 14:39:51 -0600

    Transferred state declaration from LightSwitchButton to App

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import AngryButton from './components/AngryButton';
 import CounterButton from './components/CounterButton';
 import LightSwitchButton from './components/LightSwitchButton';
 import TextRepeaterButton from './components/TextRepeaterButton';
+import { useState } from 'react';
 
 function App() {
+
+  const [light, setLight] = useState('off');
+
   return (
-    <div className="App">
+    <div className="dark">
       <h1>Fancy Buttons!</h1>
       <section>
         <AngryButton />
         <CounterButton />
-        <LightSwitchButton />
+        <LightSwitchButton light={light} setLight={setLight} />
         <TextRepeaterButton />
       </section>
     </div>
