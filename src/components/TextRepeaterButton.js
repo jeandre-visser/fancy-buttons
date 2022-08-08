7746fb520f105e13da8f7c2bbb4ce9fea2549b1b
Jeandre <jeandrev1414@gmail.com>
2022-08-08 14:08:30 -0600

    Implement handleClick function

--- src/components/TextRepeaterButton.js
+++ src/components/TextRepeaterButton.js
@@ -1,16 +1,19 @@
 import { useState } from "react";
 
 function TextRepeaterButton(props) {
-  
-
   const [repetitions, setRepetitions] = useState(1);
+
   const textArr = [];
   for (let i = 0; i < repetitions; i++) {
     textArr.push(<span key={i} >I like this text</span>)
   }
 
+  const handleClick = () => {
+    setRepetitions(repetitions + 1);
+  }
+
   return (
-    <button className="TextRepeaterButton">
+    <button className="TextRepeaterButton" onClick={handleClick} > 
     {textArr}
     </button>
   );
